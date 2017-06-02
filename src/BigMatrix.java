ee565cb5b4984f6df0ca25bd90f235aa0a31ab5b
DevWork <s-RRashid@lwsd.org>
2017-06-02 11:07:08 -0700

    i think it works now

--- src/BigMatrix.java
+++ src/BigMatrix.java
@@ -2,29 +2,42 @@ import java.util.*;
 
 public class BigMatrix {
 
-    public HashMap<Integer, HashMap<Integer, Integer>> mapMatrix;
+    // mapping of rows to columns
+    public HashMap<Integer, HashMap<Integer, Integer>> mapMatrixRowCol;
+    // mapping of columns to rows
+    public HashMap<Integer, HashMap<Integer, Integer>> mapMatrixColRow;
 
     public BigMatrix() {
-        this.mapMatrix = new HashMap<>();
+        this.mapMatrixRowCol = new HashMap<>();
     }
 
     // add @Param value at (row, col)
     public void setValue(int row, int col, int value) {
         // check to see if the row exists
-        if (mapMatrix.containsKey(row)) {
-            HashMap<Integer, Integer> temp = mapMatrix.get(row);
+        if (mapMatrixRowCol.containsKey(row)) {
+            HashMap<Integer, Integer> temp = mapMatrixRowCol.get(row);
             temp.put(col, value);
         } else {
             HashMap<Integer, Integer> temp = new HashMap<>();
             temp.put(col, value);
-            mapMatrix.put(row, temp);
+            mapMatrixRowCol.put(row, temp);
+        }
+
+        // Columns to row mapping...
+        if (mapMatrixColRow.containsKey(col)) {
+            HashMap<Integer, Integer> temp = mapMatrixColRow.get(col);
+            temp.put(row, value);
+        } else {
+            HashMap<Integer, Integer> temp = new HashMap<>();
+            temp.put(row, value);
+            mapMatrixColRow.put(col, temp);
         }
     }
 
     // get value at (row, col)
     public int getValue(int row, int col) {
-        if (mapMatrix.containsKey(row)) {
-            HashMap<Integer, Integer> temp = mapMatrix.get(row);
+        if (mapMatrixRowCol.containsKey(row)) {
+            HashMap<Integer, Integer> temp = mapMatrixRowCol.get(row);
             if (temp.containsKey(col)) {
                 return temp.get(col);
             }
@@ -33,17 +46,20 @@ public class BigMatrix {
         throw new NoSuchElementException();
     }
 
+    //@getNonEmptyRows returns a list of the rows that have at least one column with a non-zero value
     public List<Integer> getNonEmptyRows() {
         ArrayList<Integer> rows = new ArrayList<>();
-        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrix.entrySet()) {
-            rows.add(entry.getKey());
+        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrixRowCol.entrySet()) {
+            // check to see if the sum of the row is not 0
+            if (getRowSum(entry.getKey()) != 0)
+                rows.add(entry.getKey());
         }
         return rows;
     }
 
     public List<Integer> getNonEmptyRowsInColumn(int col) {
         ArrayList<Integer> rows = new ArrayList<>();
-        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrix.entrySet()) {
+        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrixRowCol.entrySet()) {
             HashMap<Integer, Integer> temp = entry.getValue();
             if (temp.containsKey(col)) {
                 rows.add(entry.getKey());
@@ -52,11 +68,12 @@ public class BigMatrix {
         return rows;
     }
 
+    //getNonEmptyCols returns a list of the columns that have at least one row with a non-zero value
     public List<Integer> getNonEmptyCols() {
         ArrayList<Integer> cols = new ArrayList<>();
         List<Integer> allRows = getNonEmptyRows();
         for (int i : allRows) {
-            HashMap<Integer, Integer> temp = mapMatrix.get(i);
+            HashMap<Integer, Integer> temp = mapMatrixRowCol.get(i);
             for (Integer o : temp.keySet()) {
                 cols.add(o);
             }
@@ -71,7 +88,7 @@ public class BigMatrix {
 
     public List<Integer> getNonEmptyColsInRow(int row) {
         ArrayList<Integer> cols = new ArrayList<>();
-        HashMap<Integer, Integer> temp = mapMatrix.get(row);
+        HashMap<Integer, Integer> temp = mapMatrixRowCol.get(row);
         for (Integer i : temp.keySet()) {
             cols.add(i);
         }
@@ -81,8 +98,8 @@ public class BigMatrix {
     // will return null if the row does not exist!!
     public int getRowSum(int row) {
         Integer sum = 0;
-        if (mapMatrix.containsKey(row)) {
-            HashMap<Integer, Integer> temp = mapMatrix.get(row);
+        if (mapMatrixRowCol.containsKey(row)) {
+            HashMap<Integer, Integer> temp = mapMatrixRowCol.get(row);
             for (Object o : temp.values())
                 sum += (Integer) o;
             return sum;
@@ -91,22 +108,20 @@ public class BigMatrix {
     }
 
     public int getColSum(int col) {
-        Integer sum = 0;
-        // this is cancerous hf...
-        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrix.entrySet()) {
-            HashMap<Integer, Integer> temp = entry.getValue();
-            // check to see if @col has a value
-            if (temp.containsKey(col)) {
-                sum += temp.get(col);
-            }
+        int sum = 0;
+        if (mapMatrixColRow.containsKey(col)) {
+            HashMap<Integer, Integer> temp = mapMatrixColRow.get(col);
+            for (Integer i : temp.values())
+                sum += i;
         }
+
         return sum;
     }
 
     // get sum of all elements in the array
     public int getTotalSum() {
         int sum = 0;
-        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrix.entrySet()) {
+        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrixRowCol.entrySet()) {
             sum += getRowSum(entry.getKey());
         }
         return sum;
@@ -115,7 +130,7 @@ public class BigMatrix {
     // i give up
     public BigMatrix multiplyByConstant(int constant) {
         BigMatrix tempMatrix = new BigMatrix();
-        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrix.entrySet()) {
+        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrixRowCol.entrySet()) {
             HashMap<Integer, Integer> temp = entry.getValue();
             for (Map.Entry<Integer, Integer> tempEntry : temp.entrySet()) {
                 tempMatrix.setValue(entry.getKey(), tempEntry.getKey(), constant * tempEntry.getValue());
@@ -125,31 +140,30 @@ public class BigMatrix {
     }
 
     public BigMatrix addMatrix(BigMatrix other) {
-        BigMatrix secondTemp = other;
-        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrix.entrySet()) {
-            if (secondTemp.mapMatrix.containsKey(entry.getKey())) {
+        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrixRowCol.entrySet()) {
+            if (other.mapMatrixRowCol.containsKey(entry.getKey())) {
                 HashMap<Integer, Integer> temp = entry.getValue();  // our matrix
-                HashMap<Integer, Integer> otherTemp = secondTemp.mapMatrix.get(entry.getKey());
+                HashMap<Integer, Integer> otherTemp = other.mapMatrixRowCol.get(entry.getKey());
                 for (Map.Entry<Integer, Integer> tempEntry : temp.entrySet()) {
                     if (otherTemp.containsKey(tempEntry.getKey())) {
                         int valueOfMatrix1 = tempEntry.getValue();
                         int valueOfMatrix2 = otherTemp.get(tempEntry.getKey());
-                        secondTemp.setValue(entry.getKey(), tempEntry.getKey(), valueOfMatrix1 + valueOfMatrix2);
+                        other.setValue(entry.getKey(), tempEntry.getKey(), valueOfMatrix1 + valueOfMatrix2);
                     } else {
-                        secondTemp.setValue(entry.getKey(), tempEntry.getKey(), tempEntry.getValue());
+                        other.setValue(entry.getKey(), tempEntry.getKey(), tempEntry.getValue());
                     }
                 }
-            }else{
-                HashMap<Integer, Integer> t = mapMatrix.get(entry.getKey());
-                secondTemp.mapMatrix.put(entry.getKey(), t);
+            } else {
+                HashMap<Integer, Integer> t = mapMatrixRowCol.get(entry.getKey());
+                other.mapMatrixRowCol.put(entry.getKey(), t);
             }
         }
-        return secondTemp;
+        return other;
     }
 
     @Override
     public String toString() {
-        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrix.entrySet()) {
+        for (Map.Entry<Integer, HashMap<Integer, Integer>> entry : mapMatrixRowCol.entrySet()) {
             HashMap<Integer, Integer> temp = entry.getValue();
             System.out.print("Row " + entry.getKey() + ": ");
             for (Map.Entry<Integer, Integer> tempEntry : temp.entrySet()) {
